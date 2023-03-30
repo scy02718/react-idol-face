992c052401fbaf303f056f484c56130d3f6e75e5
cyoo934 <cyoo934@aucklanduni.ac.nz>
2023-03-31 11:42:00 +1300

    First Commit. Simple frontend using React and Mui
    Beginning on backend work using Flask. Planning on connecting the
    
    pretrained model to the backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+//import { styled } from '@mui/material/styles';
+// import Paper from '@mui/material/Paper';
+import { useState } from 'react';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import { Box } from '@mui/system';
 
-function App() {
+function Seed(props){
+  return <Box sx={{
+    fontSize: 25,
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
+    mb:3
+  }}>SEED: {props.seed}</Box>
+}
+
+function Generate(props){
+  return <Button variant="contained" sx={{
+    '&:hover':{
+      color:'black',
+      backgroundColor: 'gray',
+    },
+    bgcolor: 'white',
+    borderRadius: 3,
+    color: 'black',
+    fontSize: 10,
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
+    lineHeight: 2,
+    padding: 1.2,
+    textAlign: 'center'
+  }} onClick={(event)=>{ event.preventDefault(); props.onChangeMode();}}>GENERATE</Button>
+}
+
+export default function BasicGrid() {
+  const [seed, setSeed] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container component="main" maxWidth="xs" sx={{
+      textAlign:'center'
+    }}>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Seed seed={seed}></Seed>
+          <ImageList sx={{ width: 256, height: 256, borderRadius: 3, marginTop: -2}} cols={1}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        </Box>
+          <Generate onChangeMode={()=>{
+            setSeed(Math.floor(Math.random() * (2**32-1)))
+          }}></Generate>
+    </Container>
   );
 }
 
-export default App;
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  }
+];
