4e1f6e21cc988f0654a4018035380b7bc94c7717
cyoo934 <cyoo934@aucklanduni.ac.nz>
2023-04-05 23:36:23 +1200

    Added the feature of keeping the seed when user requires to. This allows the user to have a variety of experience by modifying the truncation, adding noise, while keeping the seed the same

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import * as React from 'react';
 //import { styled } from '@mui/material/styles';
 // import Paper from '@mui/material/Paper';
 import { useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Button from '@mui/material/Button';
 import ImageList from '@mui/material/ImageList';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
 import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
@@ -49,15 +51,20 @@ function Generate(props){
 export default function BasicGrid() {
   const [seed, setSeed] = useState(window.localStorage.getItem('seed'));
   const [noise, setNoise] = useState(true);
+  const [keepSeed, setKeepSeed] = useState(false);
   const [psi, setPsi] = useState(0.3);
 
   const handleSliderChange = (event, newPsi) => {
     setPsi(newPsi);
   };
 
-  const handleChange = (event) => {
+  const handleNoiseChange = (event) => {
     setNoise(event.target.checked);
   };
+
+  const handleKeepSeedChange = (event) =>{
+    setKeepSeed(event.target.checked);
+  }
 
   useEffect(() => {
     setSeed(JSON.parse(window.localStorage.getItem('seed')));
@@ -85,17 +92,37 @@ export default function BasicGrid() {
             <img src={'seed'+seed+'.png'} alt="IDOL_FACE"/>
           </ImageList>
         </Box>
+        <Grid container>
+          <Grid item xs={6} sx={{ pl:8}}>
           <Generate onChangeMode={async (tempSeed)=>{
-            setSeed(tempSeed)
-            await fetch('http://127.0.0.1:7000/image?seed=' + tempSeed + '&noise=' + noise + '&psi='+psi); 
+            if (!keepSeed){
+              setSeed(tempSeed);
+              await fetch('http://127.0.0.1:7000/image?seed=' + tempSeed + '&noise=' + noise + '&psi='+psi); 
+            }
+            else {
+              await fetch('http://127.0.0.1:7000/image?seed=' + seed + '&noise=' + noise + '&psi='+psi); 
+            }
           }}></Generate>
+          </Grid>
+          <Grid item xs={6} sx={{ pl:4}}>
+          <FormGroup>
           <FormControlLabel control={
-            <Switch checked={noise} onChange={handleChange} colour='default'/>
+            <Switch checked={noise} onChange={handleNoiseChange} colour='default' size='small'/>
               } label={<Typography sx={{
                 fontSize: 12,
                 fontFamily: 'Arial',
                 fontWeight: 'bold'
-              }}>Noise</Typography>} sx={{mx : 1, mb:1.5}}/>
+              }}>Noise</Typography>} sx={{mx : 1, mt:-0.5}}/>
+          <FormControlLabel control={
+            <Switch checked={keepSeed} onChange={handleKeepSeedChange} colour='default' size='small'/>
+              } label={<Typography sx={{
+                fontSize: 12,
+                fontFamily: 'Arial',
+                fontWeight: 'bold'
+              }}>Keep Seed</Typography>} sx={{mx : 1}}/>
+          </FormGroup>
+          </Grid>
+          </Grid>
           <Typography id="psi-slider" sx={{
             fontSize: 12,
             fontFamily: 'Arial',
